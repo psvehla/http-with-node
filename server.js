05aa77186d47b5dcf0f5053cb27bb7b5ffef53ab
psvehla <psvehla@hotmail.com>
2023-02-09 23:57:35 +1100

    Getting image metadata.

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const http = require('http');
 const url = require('url');
+const services = require('./services');
 
 const server = http.createServer();
 server.on('request', (req, res) => {
     const parsedUrl = url.parse(req.url, true);
     if (req.method === 'GET' && parsedUrl.pathname === '/metadata') {
         const { id } = parsedUrl.query;
-        console.log(id);
+        const metadata = services.fetchImageMetadata(id);
+        console.log(metadata);
     }
 });
 
