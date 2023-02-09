e4da70eb6d91f94cf0f11c90bc236b188a6027a0
psvehla <psvehla@hotmail.com>
2023-02-09 23:34:53 +1100

    Logging the request.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer();
 server.on('request', (req, res) => {
-    console.log('this is an incoming request');
+    console.log(req);
 });
 
 server.listen(8080);
