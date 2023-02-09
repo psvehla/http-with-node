2fb1c65e32cc9f16e5521eb7c76764d7fb020b23
psvehla <psvehla@hotmail.com>
2023-02-09 23:38:30 +1100

    Logging request method and URL.

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer();
 server.on('request', (req, res) => {
-    console.log(req);
+    console.log(req.method, req.url);
 });
 
 server.listen(8080);
